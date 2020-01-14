197723a4e891b73b0236138c1c6baf4f8105bb45
Sarah Rodrigues <sarahrodriguesmoreira@gmail.com>
2020-01-13 22:49:30 -0300

    Add positions, movement description interfaxce

--- Projeto Unity - Avatar/Assets/Scripts/CameraDrag.cs
+++ Projeto Unity - Avatar/Assets/Scripts/CameraDrag.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine;
-using System.Collections;
 
 public class CameraDrag : MonoBehaviour {
     public Transform target;
---
+++ Projeto Unity - Avatar/Assets/Scripts/Canvas/AddPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddPosition : MonoBehaviour {
+    Vector3 offset = new Vector3(0, 80, 0);
+    public Transform positionAggregator;
+    public Button addButton;
+    public GameObject positionPrefab;
+    void Start(){
+        positionPrefab = Resources.Load("Position") as GameObject;
+    }
+
+    public void addPosition() {
+        if (positionAggregator.childCount < 5) {
+            GameObject obj = Instantiate(positionPrefab);
+            obj.transform.SetParent(positionAggregator);
+            obj.transform.position = positionAggregator.GetChild(positionAggregator.childCount - 2).position - offset;
+            obj.name = "Position " + positionAggregator.childCount.ToString();
+            obj.GetComponent<Text>().text = "Posição " + positionAggregator.childCount.ToString();
+
+            addButton.transform.position -= offset;
+        }
+    }
+}
